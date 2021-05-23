33f041765a20345cc6cd1d588d0fd4f262c6ad0e
Rodrigo Azevedo da Silva <azevedo@MacBook-de-Rodrigo.local>
2021-05-23 19:41:06 -0300

    add documentation to functions

--- project_data_warehouse/create_tables.py
+++ project_data_warehouse/create_tables.py
@@ -4,22 +4,41 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+
+    Method to drop tables.
+
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+
+    Method to create staging and dimensional tables.
+
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    """
+    
+    Method to create database.
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    """
+    config = configparser.ConfigParser()
+    config.read("dwh.cfg")
+
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config["CLUSTER"].values()
+        )
+    )
     cur = conn.cursor()
 
     drop_tables(cur, conn)
