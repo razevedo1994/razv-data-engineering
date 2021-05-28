ee875e7b493cdc9227cf03d631a6e5114e6fd21b
Rodrigo Azevedo da Silva <azevedo@MacBook-de-Rodrigo.local>
2021-05-27 23:41:20 -0300

    include staging_songs_copy

--- project_data_warehouse/sql_queries.py
+++ project_data_warehouse/sql_queries.py
@@ -102,7 +102,10 @@ staging_events_copy = (
 ).format()
 
 staging_songs_copy = (
-    """
+    """COPY staging_songs from 's3://udacity-dend/song_data'
+	    credentials 'aws_iam_role={role_arn}'
+	    compupdate off statupdate off
+	    region 'us-west-2' format as JSON 'auto';
 """
 ).format()
 
