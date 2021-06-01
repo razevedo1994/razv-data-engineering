6c21db1f7a9dd109b837a5f54aeca4b6ab4f0c61
razevedo1994 <razevedo.contato@gmail.com>
2021-06-01 10:18:42 -0300

    include docstring

--- project_data_warehouse/etl.py
+++ project_data_warehouse/etl.py
@@ -4,18 +4,30 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Method for loading data into staging tables.
+
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Method for inserting data into dimensional tables.
+
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Method to connect to the database and perform the load and insert functions.
+
+    """
     config = configparser.ConfigParser()
     config.read_file(open("dwh.cfg"))
 
