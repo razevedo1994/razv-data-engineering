e02e2590de80ad188ea519096d30749c7b1d67da
razevedo1994 <razevedo.contato@gmail.com>
2021-06-01 00:03:01 +0000

    fix error

--- project_data_warehouse/sql_queries.py
+++ project_data_warehouse/sql_queries.py
@@ -197,8 +197,8 @@ time_table_insert = """INSERT INTO time (start_time, hour, day, week, month, yea
 create_table_queries = [
     staging_events_table_create,
     staging_songs_table_create,
-    songplay_table_create,
     user_table_create,
+    songplay_table_create,
     song_table_create,
     artist_table_create,
     time_table_create,
