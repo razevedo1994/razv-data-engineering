b66e37c441c3eb1d605b2bff9e9420cc1eef4e7c
Rodrigo Azevedo da Silva <azevedo@MacBook-de-Rodrigo.local>
2021-12-12 20:42:08 -0300

    simple function to request

--- extracting_data_from_api/main.py
+++ extracting_data_from_api/main.py
@@ -1,13 +1,35 @@
-from requests import request
-from extracting_data_from_api.setup.settings import API_KEY
+import requests
+from requests import Session
+import pandas as pd
 import json
 
-base_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
-params = {"start": "1", "limit": "5000", "convert": "USD"}
 
-headers = {
-    "Accepts": "application/json",
-    "X-CMC_PRO_API_KEY": API_KEY,
-}
+def main():
+    base_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    params = {"start": "1", "limit": "100", "convert": "USD"}
 
-response = request.get(base_url, params=params)
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": API_KEY,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    response = session.get(base_url, params=params)
+
+    if response.status_code != 200:
+        if response.status_code != 404:
+            print("Error in request url.", response.status_code)
+        else:
+            print("There was a problem retrieving data: ", response.text)
+
+    else:
+        response_text = response.text
+        response_json_object = json.loads(response_text)
+        df = pd.DataFrame(response_json_object["data"])
+        df.to_csv("test.csv")
+
+
+if __name__ == "__main__":
+    main()
