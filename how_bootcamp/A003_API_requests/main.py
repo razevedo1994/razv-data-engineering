95ec177ffb7fd6f2ee394b4658b3e1b786929abd
razevedo1994 <razevedo.contato@gmail.com>
2022-01-16 01:33:53 -0300

    add logging

--- how_bootcamp/A003_API_requests/main.py
+++ how_bootcamp/A003_API_requests/main.py
@@ -1,6 +1,16 @@
 import requests
 import pandas as pd
 import backoff
+import logging
+
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch = logging.StreamHandler()
+ch.setFormatter(formater)
+log.addHandler(ch)
+
 
 url = "https://economia.awesomeapi.com.br/json/last/"
 
@@ -13,28 +23,32 @@ moedas = ["USD-BRL", "EUR-BRL", "BTC-BRL"]
     max_tries=3,
 )
 def get_price(url: str, moedas: list):
+    log.info("Initiating requests.")
     json_responses = []
     for moeda in moedas:
         endpoind = url + moeda
         response = requests.get(endpoind)
         json_responses.append(response.json())
 
+    log.info("Completed requests.")
     return json_responses
 
 
-def build_dataframe(responses: list):
+def build_csv(responses: list):
+    log.info("Starting build csv.")
     data = []
     for response in responses:
         for key, value in response.items():
             data.append(value)
 
     dataframe = pd.DataFrame(data)
+    log.info("Generating csv.")
     dataframe.to_csv("cotacoes.csv")
 
 
 def main(url: str, moedas: list):
     responses = get_price(url, moedas)
-    build_dataframe(responses)
+    build_csv(responses)
 
 
 if __name__ == "__main__":
