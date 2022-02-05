5df52e7016c846e01cf5d382968cd2fc396a5a84
razevedo1994 <razevedo.contato@gmail.com>
2022-02-05 16:45:22 -0300

    refactor code and modules

---
+++ how_bootcamp/A005_OOP/main.py
@@ -0,0 +1,20 @@
+import datetime
+import time
+from schedule import repeat, every, run_pending
+from ingestors import DaySummaryIngestor
+from writers import DataWriter
+
+if __name__ == "__main__":
+    day_summary_ingestor = DaySummaryIngestor(
+        writer=DataWriter,
+        coins=["BTC", "ETH", "LTC"],
+        default_start_date=datetime.date(2021, 6, 1),
+    )
+
+    @repeat(every(1).seconds)
+    def job():
+        day_summary_ingestor.ingest()
+
+    while True:
+        run_pending()
+        time.sleep(0.5)
---
+++ how_bootcamp/A005_OOP/writers.py
@@ -0,0 +1,28 @@
+from exceptions.custom_exception import (
+    DataTypeNotSupportedForIngestionException,
+)
+import json
+import os
+from typing import List
+import datetime
+
+
+class DataWriter:
+    def __init__(self, coin: str, api: str) -> None:
+        self.coin = coin
+        self.api = api
+        self.filename = f"{self.api}/{self.coin}/{datetime.datetime.now()}.json"
+
+    def _write_row(self, row: str) -> None:
+        os.makedirs(os.path.dirname(self.filename), exist_ok=True)
+        with open(self.filename, "a") as f:
+            f.write(row)
+
+    def write(self, data: list):
+        if isinstance(data, dict):
+            self._write_row(json.dumps(data) + "\n")
+        elif isinstance(data, List):
+            for element in data:
+                self.write(element)
+        else:
+            raise DataTypeNotSupportedForIngestionException(data)
