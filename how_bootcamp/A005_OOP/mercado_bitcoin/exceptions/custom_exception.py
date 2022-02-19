0df4a275debf5b315c91f6e78e88d2c2f76acf97
razevedo1994 <razevedo.contato@gmail.com>
2022-02-19 11:25:59 -0300

    remove files

--- how_bootcamp/A005_OOP/mercado_bitcoin/exceptions/custom_exception.py
+++
@@ -1,5 +0,0 @@
-class DataTypeNotSupportedForIngestionException(Exception):
-    def __init__(self, data) -> None:
-        self.data = data
-        self.message = f"Data type {type(data)} is not supported for ingestion"
-        super().__init__(self.message)
