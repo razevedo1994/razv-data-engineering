96371c19c2c0763298178f2d0a0ac8894c14bbe2
razevedo1994 <razevedo.contato@gmail.com>
2022-05-25 00:04:17 -0300

    feat: save results of query in csv file

--- extract/extract_data_from_mysql/extract_mysql_full.py
+++ extract/extract_data_from_mysql/extract_mysql_full.py
@@ -11,13 +11,25 @@ username = parser.get("mysql_config", "username")
 dbname = parser.get("mysql_config", "database")
 password = parser.get("mysql_config", "password")
 
-conn = pymysql.connect(host=hostname,
-        user=username,
-        password=password,
-        db=dbname,
-        port=int(port))
+conn = pymysql.connect(
+    host=hostname, user=username, password=password, db=dbname, port=int(port)
+)
 
 if conn is None:
     print("Error connecting to the MySQL databse")
 else:
     print("MySQL connection established!")
+
+query = "SELECT * FROM Orders.Orders;"
+local_filename = "order_extracted.csv"
+
+cursor = conn.cursor()
+cursor.execute(query)
+results = cursor.fetchall()
+
+with open(local_filename, "w") as file_:
+    csv_w = csv.writer(file_, delimiter="|")
+    csv_w.writerows(results)
+    file_.close()
+    cursor.close()
+    conn.close()
