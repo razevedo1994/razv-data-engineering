a4b3b7faab33e54e3366933d5aab5803b8aac93f
razevedo1994 <razevedo.contato@gmail.com>
2022-05-24 22:58:31 -0300

    feat: commands to create and populate table

---
+++ extract/extract_data_from_mysql/create_table.sql
@@ -0,0 +1,18 @@
+-- Create table
+CREATE TABLE Orders (
+	OrderId int,
+	OrderStatus varchar(30),
+	LastUpdated timestamp
+);
+
+-- Populate table
+INSERT INTO Orders.Orders 
+	VALUES(1, 'Backordered', '2020-06-01 12:00:00');
+INSERT INTO Orders.Orders 
+	VALUES(1, 'Shipped', '2020-06-09 12:00:25');
+INSERT INTO Orders.Orders 
+	VALUES(2, 'Shipped', '2020-07-11 3:05:00');
+INSERT INTO Orders.Orders 
+	VALUES(1, 'Shipped', '2020-06-09 11:50:00');
+INSERT INTO Orders.Orders 
+	VALUES(1, 'Shipped', '2020-07-12 12:00:00');
