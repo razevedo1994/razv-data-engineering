299858f1c8d49bb8690b6c0e5e1db9e8c5345183
razevedo1994 <razevedo.contato@gmail.com>
2022-05-24 23:56:37 -0300

    feat: create mysql connection

---
+++ extract/extract_data_from_mysql/extract_mysql_full.py
@@ -0,0 +1,23 @@
+import pymysql
+import csv
+import boto3
+import configparser
+
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+hostname = parser.get("mysql_config", "hostname")
+port = parser.get("mysql_config", "port")
+username = parser.get("mysql_config", "username")
+dbname = parser.get("mysql_config", "database")
+password = parser.get("mysql_config", "password")
+
+conn = pymysql.connect(host=hostname,
+        user=username,
+        password=password,
+        db=dbname,
+        port=int(port))
+
+if conn is None:
+    print("Error connecting to the MySQL databse")
+else:
+    print("MySQL connection established!")
