0a821b2e261fc091578e4e82807358cef21e8ec3
razevedo1994 <razevedo.contato@gmail.com>
2022-06-03 21:54:05 -0300

    refactor: refatorando prog2

--- linux/curso/mod1/prog2.sh
+++ linux/curso/mod1/prog2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+# Um laço que conte de 0 a 10
+# A cada iteração, verifique se o número é divisível por 2
+# Se ele for divisível por 2, escreva uma mensagem na tela confirmando isso
 
-for (( i = 0; i < 10; i++ )); do
-  if [ $(( $i % 2 )) = 0 ]
-  then
-    echo "$i eh divisivel por 2."
-  fi
+for i in $(seq 0 10); do
+  [ $(( $i % 2 )) -eq 0 ] && echo "Número $i é divisível por 2"
 done
