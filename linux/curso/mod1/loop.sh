469afbd0ed9766ec3d9482b536c807b4eb5df355
razevedo1994 <razevedo.contato@gmail.com>
2022-06-03 21:21:30 -0300

    add: mais formas de loop

--- linux/curso/mod1/loop.sh
+++ linux/curso/mod1/loop.sh
@@ -9,3 +9,21 @@ echo "==== For 2 (seq)"
 for i in $(seq 10); do
   echo $i
 done
+
+echo "==== For 3 (array)"
+Frutas=(
+'Laranja'
+'Pera'
+'Abacaxi'
+'Melancia'
+)
+for i in "${Frutas[@]}"; do # @ significa todos os items do array
+  echo "$i"
+done
+
+echo "==== While"
+contador=0
+while [[ $contador -lt ${#Frutas[@]} ]]; do
+  echo ${Frutas[$contador]}
+  contador=$(($contador+1))
+done
