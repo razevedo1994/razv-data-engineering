123a0291ba3582efb2029c982d50db9c08674d99
razevedo1994 <razevedo.contato@gmail.com>
2023-01-21 08:04:10 -0300

    Reorganize files

--- linux/curso/mod1/prog1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#Validar se o primeiro parâmetro enviado é maior que 10
-#Se for maior, mostre uma mensagem na tela com o nome do script e seu respectivo PID de execução
-
-
-
-[ $1 -gt 10 ] && echo "Programa: $0 PID: $$"
--- linux/curso/mod1/prog2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Um laço que conte de 0 a 10
-# A cada iteração, verifique se o número é divisível por 2
-# Se ele for divisível por 2, escreva uma mensagem na tela confirmando isso
-
-for i in $(seq 0 10); do
-  [ $(( $i % 2 )) -eq 0 ] && echo "Número $i é divisível por 2"
-done
