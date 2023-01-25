7f9f4d7e2087268a14caf17a7ab4fe9980e05da7
razevedo1994 <razevedo.contato@gmail.com>
2023-01-25 20:43:15 -0300

    feat: iam terraform

---
+++ 15_aws/write_partitioned_parquet_files_with_glue/infra/iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "glue_job" {
+    name               = "razv-glue-job-role"
+    path               = "/"
+    description        = "Provides write permissions to CloudWatch Logs and S3 Full Access"
+    assume_role_policy = file("./permissions/role_glue_jobs.json")
+  
+}
+
+resource "aws_iam_policy" "glue_job" {
+    name        = "${local.prefix}-glue-job-policy"
+    path        = "/"
+    description = "Provides write permissions to CloudWatch Logs and S3 Full Access"
+    policy      = file("./permissions/policy_glue_jobs.json")
+  
+}
+
+resource "aws_iam_role_policy_attachment" "glue_job" {
+  role       = aws_iam_role.glue_job.name
+  policy_arn = aws_iam_policy.glue_job.arn
+}
