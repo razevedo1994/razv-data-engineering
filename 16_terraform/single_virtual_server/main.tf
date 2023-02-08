11071edb90f3ef52341152ab9f7e2f2309ff0ab8
razevedo1994 <razevedo.contato@gmail.com>
2023-02-07 23:21:14 -0300

    feat: resources to deploy a simple web server

--- 16_terraform/single_virtual_server/main.tf
+++ 16_terraform/single_virtual_server/main.tf
@@ -1,8 +1,43 @@
-provider "aws" {
-    region = "us-east-2"
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
-resource "aws_instance" "ec2_example" {
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
     ami             = "ami-0fb653ca2d3203ac1"
     instance_type   = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    user_data_replace_on_change = true
+    
+    tags = {
+      Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
