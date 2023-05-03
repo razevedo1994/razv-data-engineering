3ddeaac0fbf36420e8da3b1fe608abf8c7c111a7
razevedo1994 <razevedo.contato@gmail.com>
2023-05-02 21:07:38 -0300

    update questions file

--- 19_SQL/udacity-introduction-to-sql/questions.sql
+++ 19_SQL/udacity-introduction-to-sql/questions.sql
@@ -407,3 +407,43 @@ GROUP BY a.id, a.name, w.channel
 ORDER BY use_of_channel DESC
 LIMIT 10;
 
+
+/* Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least. Do you notice any trends in the yearly sales totals? */
+
+SELECT	DATE_PART('year', occurred_at) AS year,
+		SUM(total_amt_usd) AS total_dollars
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC
+
+
+/* Which month did Parch & Posey have the greatest sales in terms of total dollars? Are all months evenly represented by the dataset? */
+
+SELECT	DATE_PART('month', occurred_at) AS order_month,
+		SUM(total_amt_usd) AS total_spent
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC
+
+
+/* Which year did Parch & Posey have the greatest sales in terms of the total number of orders? Are all years evenly represented by the dataset? */
+
+SELECT	DATE_PART('year', occurred_at) AS order_year,
+		COUNT(*) AS total_sales
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC
+
+
+/* In which month of which year did Walmart spend the most on gloss paper in terms of dollars? */
+
+SELECT	DATE_TRUNC('month', orders.occurred_at) AS order_date,
+		SUM(orders.gloss_amt_usd) AS total_spent
+FROM orders
+JOIN accounts
+ON accounts.id = orders.account_id
+WHERE accounts.name = 'Walmart'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1
