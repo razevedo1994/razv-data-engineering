07d75fbbafccce91ae1788add2e8843b7e436ac0
razevedo1994 <razevedo.contato@gmail.com>
2023-05-03 00:02:05 -0300

    feat: add cli commands (shell, list users)

---
+++ 15_aws/data_ingestion_with_lambda/myfinance-api/myfinance/cli.py
@@ -0,0 +1,49 @@
+import typer
+from rich.console import Console
+from rich.table import Table
+from sqlmodel import Session, select
+from .config import settings
+from .db import engine
+from .models import User
+
+
+main = typer.Typer(name="Myfinance CLI")
+
+
+@main.command()
+def shell():
+    """Opens interactive shell"""
+    _vars = {
+        "settings": settings,
+        "engine": engine,
+        "select": select,
+        "session": Session(engine),
+        "User": User,
+    }
+    typer.echo(f"Auto imports: {list(_vars.keys())}")
+    try:
+        from IPython import start_ipython
+
+        start_ipython(
+            argv=["--ipython-dir=/tmp", "--no-banner"], user_ns=_vars
+        )
+    except ImportError:
+        import code
+
+        code.InteractiveConsole(_vars).interact()
+
+
+@main.command()
+def user_list():
+    """Lists all users"""
+    table = Table(title="Myfinance Users")
+    fields = ["username", "email"]
+    for header in fields:
+        table.add_column(header, style="magenta")
+
+    with Session(engine) as session:
+        users = session.exec(select(User))
+        for user in users:
+            table.add_row(user.username, user.email)
+
+    Console().print(table)
