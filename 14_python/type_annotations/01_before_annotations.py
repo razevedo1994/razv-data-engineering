5ef23a9499a47a677f61628fa93e6dfd96069f70
razevedo1994 <razevedo.contato@gmail.com>
2023-07-13 22:40:56 -0300

    Exercise of type annotations

---
+++ 14_python/type_annotations/01_before_annotations.py
@@ -0,0 +1,20 @@
+def calculate_average(numbers):
+  total = sum(numbers)
+  return total / len(numbers)
+
+def calculate_total_sales(sales):
+  return sum(sales.values())
+
+def main():
+  data = [1, 2, 3, 4, 5]
+  average = calculate_average(data)
+  print("The average is:", average)
+
+  sales_data = {"product_a": 100, "product_b": 250, "product_c": 80,  
+                "product_d": 150}
+  print("Sales data for product C:", sales_data["product_c"])
+  total_sales = calculate_total_sales(sales_data)
+  print("Total sales:", total_sales)
+
+if __name__ == "__main__":
+  main()
