602c46033ef30ad82d03e64ff7319aab54fc23d5
razevedo1994 <razevedo.contato@gmail.com>
2023-10-07 21:07:10 -0300

    python: Iterator abstraction

---
+++ 14_python/iterators/iterator_abstraction.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Iterable
+
+
+@dataclass(frozen=True)
+class LineItem:
+    price: int
+    quantity: int
+
+    def total_price(self) -> int:
+        return self.price * self.quantity
+    
+
+def print_totals(items: Iterable[LineItem]) -> None:
+    for item in items:
+        print(item.total_price())
+
+
+def main() -> None:
+    line_items = {
+        LineItem(1, 2),
+        LineItem(3, 4),
+        LineItem(5, 6)
+    }
+    print_totals(line_items)
+
+
+if __name__ == "__main__":
+    main()
