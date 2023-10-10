0bd529f14082800b871da9faee0651f41d5704b5
razevedo1994 <razevedo.contato@gmail.com>
2023-10-09 22:48:59 -0300

    delete folder

--- 15_aws/data_ingestion_with_lambda/myfinance-api/myfinance/app.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI(
-    title="Myfinance",
-    version="0.1.0",
-    description="Myfinance is a personal finance app"
-)
--- 15_aws/data_ingestion_with_lambda/myfinance-api/myfinance/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from sqlmodel import SQLModel
-from .user import User
-
-
-__all__ = ["SQLModel", "User"]
